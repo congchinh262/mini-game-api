b23e13c414e05cfd12bef468b7c31bcd89458ff6
congchinh262 <congchinh262@gmail.com>
2022-12-27 16:43:58 +0700

    change start script

--- src/azure.ts
+++ src/azure.ts
@@ -3,7 +3,7 @@ import { ClientSecretCredential, DefaultAzureCredential } from "@azure/identity"
 import { AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID, KEY_VAULT_NAME } from "../conf";
 
 export namespace Azure {
-  const credential = new ClientSecretCredential("fd98a8b7-59d7-4921-903e-2d3011f70e81","4f8a47f5-620a-4fc4-97fe-35639af177af","APC8Q~sSC7FN5ilDuDeqlWLEDKTi72v3zUiJ2cQ6");
+  const credential = new ClientSecretCredential(process.env.AZURE_TENANT_ID!,process.env.AZURE_CLIENT_ID!,process.env.AZURE_CLIENT_SECRET!);
   const url = "https://" + KEY_VAULT_NAME + ".vault.azure.net";
   const client = new SecretClient(url, credential);
   export const getSecrets = async (name: string) => {
